cf41a65312146413e97df53c88663137e04dd805
Виталий Вайти <vit@vayti.ru>
2021-03-24 11:17:35 +0400

    Update parser.php

--- parser.php
+++ parser.php
@@ -1,3 +1,27 @@
 <?
+$delimiter = ';';
+
+$csv = file_get_contents('bd.csv');
+$rows = explode(PHP_EOL, $csv);
+
+$arData = [];
+
+foreach ($rows as $row)
+{
+    $arRow = explode($delimiter, $row);
+    $arRow = [
+        'date' => $arRow[0],
+        'kbk' => $arRow[1],
+        'address' => $arRow[2],
+        'sum' => $arRow[3],
+        'count' => 1
+    ];
+    if($arData[$arRow['kbk']]) {
+        $arData[$arRow['kbk']]->sum += $arRow['sum'];
+        $arData[$arRow['kbk']]->count += 1;
+    }else{
+        $arData[$arRow['kbk']] = (object)$arRow;
+    }
+}
 
 ?>
